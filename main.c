8ae545dc886970d7e279a4705f961054274e3665
Emily <emily.quelal.gonzalez@udla.edu.ec>
2026-04-28 11:43:52 -0500

    sumar.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (int argc, char const *argv[]) 
+{
+
+    int a, b;
+    printf("Ingrese 1er numero:");
+    scanf("%d", &a);
+    printf("Ingrese 2do numero: ");
+    scanf("%d", &b);
+    sumar(a,b);
+    return 0;
+
+}
